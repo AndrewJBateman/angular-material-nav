c29a4a901ad3a139201e69332e706ddfbc0cc01e
AndrewJBateman <gomezbateman@yahoo.com>
2021-12-10 18:00:46 +0100

    feat: Nav menus improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -21,12 +21,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Component, ViewChild } from "@angular/core";
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { MatSidenav } from "@angular/material/sidenav";
+import { delay } from "rxjs/operators";
 
 @Component({
   selector: "app-navigation",
@@ -9,9 +9,26 @@ import { map } from "rxjs/operators";
   styleUrls: ["./navigation.component.css"],
 })
 export class NavigationComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(map((result) => result.matches));
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver) {}
+
+  // breakpoint observer checks if width is over 700px.
+  // If so use 'over' mode and close side nav
+  // If less than 700px use 'side' mode & open side nav
+  ngAfterViewInit() {
+    this.observer
+      .observe(["(max-width: 700px)"])
+      .pipe(delay(1)) // delay 1mS
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = "over";
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = "side";
+          this.sidenav.open();
+        }
+      });
+  }
 }
